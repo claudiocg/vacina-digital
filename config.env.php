30de85a9981bea7a2be6240c3ef3a29dac863aca
Edgar Yonemura <edgar.yonemura@gmail.com>
2016-09-30 12:52:42 -0300

    Adicionado configuração de ambiente

--- config.env.php
+++ config.env.php
@@ -9,5 +9,8 @@ return [
 		'options' => [
 			PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
 		]
-	]
+	],
+    // production: desabilita o debug de erro 
+    // development: habilita o debug de erro
+    'env' => 'development'
 ];
